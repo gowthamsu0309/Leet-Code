fd64355413e9c39ea09beaa74bda19f1979309d0
Gowtham S U <mrgowtham0309_bit27@mepcoeng.ac.in>
2026-06-07 11:21:39 +0530

    Time: 1 ms (62.75%) | Memory: 43.4 MB (32.15%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,19 +1,27 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0){
+        if(strs.length == 0){
             return "";
         }
-        String prefix = strs[0];
-        for(int i = 0;i < strs.length;i++){
+        if(strs.length == 1){
+            return strs[0];
+        }
+        Arrays.sort(strs);
 
-            while(strs[i].indexOf(prefix) != 0){
-                prefix = prefix.substring(0, prefix.length() - 1);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
 
-                if(prefix.isEmpty()){
-                    return "";
-                }
+        int p1 = 0;
+        int p2 = 0;
+        while(p1 < first.length() && p2 < last.length()){
+            if(first.charAt(p1) == last.charAt(p2)){
+                p1++;
+                p2++;
+            }
+            else{
+                break;
             }
         }
-        return prefix;
+        return first.substring(0,p1);
     }
 }
