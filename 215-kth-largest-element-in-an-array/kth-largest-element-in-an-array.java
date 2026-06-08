a3c543a592ca2d474b0ad8768b24a636c80b3995
Gowtham S U <mrgowtham0309_bit27@mepcoeng.ac.in>
2026-06-08 16:57:57 +0530

    Time: 44 ms (73.64%) | Memory: 78.4 MB (5.09%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,45 +1,44 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        mergeSort(nums,0,nums.length - 1);
-        return nums[nums.length - k];
+        
+        mergeSort(nums,0,nums.length-1);
+
+        return nums[k-1];
     }
-    public void mergeSort(int [] nums,int start,int end){
+    void mergeSort(int [] nums,int start,int end){
         if(start < end){
             int mid = (start + end) / 2;
 
             mergeSort(nums,start,mid);
             mergeSort(nums,mid + 1,end);
 
-            merge(nums, start, mid, end);
+            merge(nums,start,mid,end);
         }
     }
-    public void merge(int [] nums, int start, int mid, int end){
-        int [] a = new int[end - start+1];
+    void merge(int[] nums,int start,int mid,int end){
+        int [] a = new int[end - start + 1];
         int i = start,j = mid + 1,k = 0;
+
         while(i <= mid && j <= end){
-            if(nums[i] < nums[j]){
+            if(nums[i] > nums[j]){
                 a[k] = nums[i];
-                i++;
-                k++;
+                i++;k++;
             }
             else{
                 a[k] = nums[j];
-                j++;
-                k++;
+                j++;k++;
             }
         }
         if(i > mid){
             while(j <= end){
                 a[k] = nums[j];
-                j++;
-                k++;
+                j++;k++;
             }
         }
         else{
             while(i <= mid){
                 a[k] = nums[i];
-                i++;
-                k++;
+                i++;k++;
             }
         }
         k = 0;
@@ -47,6 +46,5 @@ class Solution {
             nums[l] = a[k];
             k++;
         }
-
     }
 }
